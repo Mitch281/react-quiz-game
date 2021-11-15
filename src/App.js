ec582004594f9d5711b96e184ea1277f4297dc73
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-11-15 23:30:41 +1100

    Successfully fetched information from API and set the state as that data.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Question from "./components/Question";
 import Timer from "./components/Timer";
 import StartGame from "./components/StartGame";
 
 function App() {
-  const [question, setQuestion] = useState([
-    {
-      id: 0,
-      questionText: "What is 1+1?",
-      options: {1: "option1", 2:"option2", 3:"option3", 4:"option4"},
-      answer: "option2"
-    }
-  ]);
-
+  const [questionData, setQuestionData] = useState("");
   const [timeLeft, setTimeLeft] = useState(59);
   const [startGame, setStartGame] = useState(false);
 
-  //fetch("https://opentdb.com/api.php?amount=10").then(res => res.json()).then((data);
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then(res => res.json())
+    .then(data => setQuestionData((questionData) => questionData = data));
+  }, [])
 
+  console.log(questionData);
 
   return (
     <div className="App">
       {!startGame ? <StartGame startGame={startGame} onStart={setStartGame} /> : ""}
       <div id="question-timer-container">
-        {startGame ? <Question question={question[0].questionText} /> : ""}
+        {startGame ? <Question question={"test"} /> : ""}
         {startGame ? <Timer timeLeft={timeLeft} /> : ""}
       </div>
     </div>
