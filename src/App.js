1c8fcb210b2dad354d330226b37a21ae17c4279f
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-11-16 11:31:50 +1100

    Added check for loaded data.

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ function App() {
   const [questionData, setQuestionData] = useState("");
   const [timeLeft, setTimeLeft] = useState(59);
   const [startGame, setStartGame] = useState(false);
+  let dataLoaded = false;
 
+  // Fetch questions and answers from api.
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10")
     .then(res => res.json())
     .then(data => setQuestionData((questionData) => questionData = data));
   }, [])
 
-  console.log(questionData);
+  // Check if we have successfully loaded our question data from api. If not, do not render components yet!
+  if (questionData !== "") {
+    dataLoaded = true;
+  }
 
   return (
     <div className="App">
       {!startGame ? <StartGame startGame={startGame} onStart={setStartGame} /> : ""}
       <div id="question-timer-container">
-        {startGame ? <Question question={"test"} /> : ""}
-        {startGame ? <Timer timeLeft={timeLeft} /> : ""}
+        {startGame && dataLoaded ? <Question question={questionData.results[0].question} /> : ""}
+        {startGame && dataLoaded ? <Timer timeLeft={timeLeft} /> : ""}
       </div>
     </div>
   );
